731a9f9a22cc84a31f10ad7a7a54d2fa7c233958
Jonas Wieser <jonas.wieser@htlwy.at>
2024-09-24 15:17:35 +0200

    task-management JavaScript file

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("[submit]").addEventListener("click",AddTodo);
+
+function AddTodo(){
+    let description = document.getElementById("[input]").value;
+     
+}
+function DeleteTodo(){
+
+}
