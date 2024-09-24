7fdf1fba22da94b3970d7895ec662b495ea3639c
Jonas Wieser <jonas.wieser@htlwy.at>
2024-09-24 18:38:25 +0200

    Update_2

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function AddTodo(){
     let description = document.getElementById("input").value;
 
     let divElement = document.createElement("div"); //div Element für TODO
-    divElement.setAttribute("id", "todo" + toDoCount);
+    divElement.setAttribute("id", "todo," + toDoCount);
     divElement.setAttribute("class", "toDoElement")
 
     let checkBoxElement = document.createElement("input"); //Checkbox
     checkBoxElement.setAttribute("type", "checkbox");
     checkBoxElement.setAttribute("id", "checkbox" + toDoCount);
-    checkBoxElement.setAttribute("onclick", 'ClickedCheckBox("todo'+ toDoCount + '")')
+    checkBoxElement.setAttribute("onclick", 'ClickedCheckBox("todo,'+ toDoCount + '")')
     divElement.appendChild(checkBoxElement);
 
     let descriptionElement = document.createElement("div"); //Beschreibung
@@ -23,28 +23,28 @@ function AddTodo(){
     deleteButton.setAttribute("class", "deleteButton");
     deleteButton.innerHTML = "Löschen";
     deleteButton.setAttribute("id", "delete"+toDoCount);
-    deleteButton.setAttribute("onclick", 'DeleteTodo("todo' + toDoCount + '")');
+    deleteButton.setAttribute("onclick", 'DeleteTodo("todo,' + toDoCount + '")');
     divElement.appendChild(deleteButton);
 
-    document.getElementById("output").appendChild(divElement);
+    document.getElementById("output").appendChild(divElement);  //TODO hinzufügen
     toDoCount++;
 }
 
 function DeleteTodo(deleteId){
     if(confirm("Möchten Sie diese Aufgabe wirklich löschen?")) {
-        document.getElementById(deleteId).innerHTML = "";
+        document.getElementById(deleteId).remove();
     } 
 }
 
 function ClickedCheckBox(checkBoxId){
-    let str = checkBoxId.slice(-1);
-    let content = document.getElementById("desc" + str).innerHTML;
+    let str = checkBoxId.split(",");
+    let content = document.getElementById("desc" + str[1]).innerHTML;
     if(content.search('<s>') == -1){
-        document.getElementById("desc" + str).innerHTML = '<s>' + content + '</s>';
+        document.getElementById("desc" + str[1]).innerHTML = '<s>' + content + '</s>';
     }
     else {
         content = content.replace('<s>', '');
         content = content.replace('</s>', '');
-        document.getElementById("desc" + str).innerHTML = content;
+        document.getElementById("desc" + str[1]).innerHTML = content;
     }
 }
