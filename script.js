3ba2982821b5237e4099bc3f5e3e7a242d1991c4
Jonas Wieser <jonas.wieser@htlwy.at>
2024-09-25 10:25:13 +0200

    EverythingWorksNow

--- script.js
+++ script.js
@@ -62,3 +62,30 @@ function ClickedCheckBox(checkBoxId){   //durchstreichen
         document.getElementById("desc" + str[1]).innerHTML = content;
     }
 }
+
+function save(taskCounter, saveOutputDom = true) {
+    try {
+        localStorage.setItem("taskCounter", taskCounter);
+        if (saveOutputDom) {
+            let dom = document.getElementById("output");
+            localStorage.setItem("dom", dom.innerHTML);
+        }
+        return 0;
+    } catch (error) {
+        console.error(error);
+        return 1;
+    }
+}
+
+function load(loadOutputDom = true) {
+    try {
+        if (loadOutputDom) {
+            let dom = document.getElementById("output");
+            dom.innerHTML = localStorage.getItem("dom");
+        }
+        return localStorage.getItem("taskCounter");
+    } catch (error) {
+        console.error(error);
+        return -1;
+    }
+}
